c059360fb37e6bcecbdd2db9eb429318d69cdcfd
Joan-yy <147325024+Joan-yy@users.noreply.github.com>
2024-06-09 12:51:45 +0800

    Update Team.java

--- Team.java
+++ Team.java
@@ -120,7 +120,7 @@ public class Team {
                        .filter(p->p.getPosition().equalsIgnoreCase(position))
                        .collect(Collectors.toList());
         }
-    /*
+    
     public static void calculateAndPrintRankings() {
 
         HashMap<String, Double> playerScores = new HashMap<>();
@@ -134,8 +134,9 @@ public class Team {
 
         // Sort players based on composite score
         List<Map.Entry<String, Double>> sortedEntries = new ArrayList<>(playerScores.entrySet());
-        sortedEntries.sort(Map.Entry.comparingByValue(Comparator.reverseOrder()));
-
+        //sortedEntries.sort(Map.Entry.comparingByValue(Comparator.reverseOrder()));
+        insertionSort(sortedEntries);
+        
         // Print rankings
         System.out.println(" -- Player Performance Ranking -- ");
         int rank = 1;
@@ -165,28 +166,8 @@ public class Team {
 
         return compositionScore;
     }
-    */
 
-    public static List<Map.Entry<String, Double>> calculateRankings(List<Players> players) {
-
-        HashMap<String, Double> playerScores = new HashMap<>();
-
-        // Calculate composite score for each player
-        for (Players player : players) {
-            String name = player.getFirstName() + player.getLastName();
-            double compositionScore = compositeScore(player.getPosition(), player);
-            playerScores.put(name, compositionScore);
-        }
-
-        // Sort players based on composite score
-        List<Map.Entry<String, Double>> sortedEntries = new ArrayList<>(playerScores.entrySet());
-        //sortedEntries.sort(Map.Entry.comparingByValue(Comparator.reverseOrder()));
-        insertionSort(sortedEntries);
-
-        return sortedEntries;
-    }
-
-    public static void insertionSort(List<Map.Entry<String, Double>> list) {
+        public static void insertionSort(List<Map.Entry<String, Double>> list) {
         // To iterate through all the elements
         for (int i = 1; i < list.size(); i++) {
             Map.Entry<String, Double> currentEntry = list.get(i);
@@ -216,37 +197,7 @@ public class Team {
         }
     }
  */
-
-    public static void printRankings(List<Map.Entry<String, Double>> sortedPlayers) {
-        System.out.println(" -- Player Performance Ranking -- ");
-        int rank = 1;
-        for (Map.Entry<String, Double> entry : sortedPlayers) {
-            System.out.println("Player : " + entry.getKey());
-            System.out.printf("Composite Score : %.2f\n" ,entry.getValue());
-            System.out.println("Rank : " + rank);
-            System.out.println();
-            rank++;
-        }
-    }
-
-    private static double compositeScore(String position, Players player) {
-        double compositionScore = 0;
-        double points = player.getPoints();
-        double assists = player.getAssists();
-        double rebounds = player.getRebounds();
-        double steals = player.getSteals();
-        double blocks = player.getBlocks();
-
-        if (position.equalsIgnoreCase("Forward"))
-            compositionScore = ((points / 23 + steals / 1.5 + rebounds / 7 + blocks / 0.3 + assists / 8) * 100) / 5;
-        else if (position.equalsIgnoreCase("Center"))
-            compositionScore = ((points / 10 + blocks / 0.5 + rebounds / 12 + assists / 8 + steals / 1.3) * 100) / 5;
-        else if (position.equalsIgnoreCase("Guard"))
-            compositionScore = (((points / 18) + (assists / 9) + (steals / 1.3) + (rebounds / 5) + (blocks / 0.3)) * 100) / 5;
-
-        return compositionScore;
-    }
-
     
+
     }
 
